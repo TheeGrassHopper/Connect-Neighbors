ac18d3e9c42d75df9fd6b4b4ddccedc468073c17
TheeGrassHopper <almortada88@gmail.com>
2015-07-22 02:22:21 -0700

    added some styling ass well as images to messages but not displaying
    ;

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,9 @@ class Message < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 	has_many :paperclip_images
+	validates :title, presence: true
+  	validates :description, presence: true
+  	validates_associated :comments
 end
+
+
--- app/models/paperclip_image.rb
+++ app/models/paperclip_image.rb
@@ -1,6 +1,10 @@
 class PaperclipImage < ActiveRecord::Base
+
   has_attached_file :image, styles: { medium: "300x300>", small: '140x140>', thumb: '64x64!' }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ , presence: true
+  validates_attachment :image, presence: true,
+  	:content_type => { :content_type => ["image/jpeg", "image/png"], :message => "must be a jpeg or png" },
+  	:size => { :in => 0..1.megabytes, :message => "must be less than 1 MB" }
   belongs_to :users
   belongs_to :messages
 end
