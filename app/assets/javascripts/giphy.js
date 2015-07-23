cdd1565fe7baf0cae921667118a292b59ec5e14c
TheeGrassHopper <almortada88@gmail.com>
2015-07-23 08:19:04 -0700

    styling

--- app/assets/javascripts/giphy.js
+++ app/assets/javascripts/giphy.js
@@ -1,15 +1,8 @@
 $(document).ready(function(){
-	// var offset = 0;
-	// var q = $("#q").val();
 	$("#search").submit(function(event){
 		event.preventDefault();
-		// alert("Loading!");
-			// var apiKey = "dc6zaTOxFJmzC"
-			// $('#results').html('');
 			gif();
 	});	
-	// $("#results button").click(function(){
-		// alert("check!");
 		$("#results button").click(function(){
 			gif();
 		});
@@ -25,14 +18,7 @@ var offset = 0;
 
 function gif(){
 	var q = $("#q").val();
-	$.ajax({
-		type: 'get',
-		url: 'http://api.giphy.com/v1/gifs/search?q=' + q + '&api_key=dc6zaTOxFJmzC&offset=' + offset + '&limit=1',
-		dataType: 'json',
-		//  data: {
- 		// 	q: q,
- 		// 	api_key: apiKey,	      
-  		// 	},
+	$.ajax({type: 'get',url: 'http://api.giphy.com/v1/gifs/search?q=' + q + '&api_key=dc6zaTOxFJmzC&offset=' + offset + '&limit=1',dataType: 'json',
         success: function(data, textStatus, jqXHR){
     	console.log(data);
     	var gifs = data.data;
@@ -41,12 +27,8 @@ function gif(){
         		var gif = gifs[i];
         		var gifPic = gif.images.fixed_height.url;
         		var html = "<li><img src=" + gifPic + "></li>";
-        		// var html = "<ul><li><img scr=" + gifPic + "></img></ul></li>"; 
-        		// $("#results").prepend(html);
         		$("#results").before(html);
-	        
-	        	// $("results").infinitescroll('scroll')
-        	}	// alert("check");	 
+        	}
         }	       	
     });			
 }
